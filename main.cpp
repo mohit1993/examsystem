7cfb4fb951fa76e31c966645515e9ddb32a1d9db
Mohit Goyal <mohitgoyal707@gmail.com>
2015-03-28 18:56:37 +0530

    added getline function to make name inputs easy:

--- main.cpp
+++ main.cpp
@@ -4,18 +4,18 @@
 #include <string>
 using namespace std;
 
-/*the linux below help to detect whetther the system is linux or windows and
- * hence define a variable CLEAR accordingly for clear function
- * on windows clear screen is done using system("cls")
+/*the lines below help to detect whether the system is linux or windows and
+ * hence define a variable CLEAR accordingly for clear function.
+ * On windows clear screen is done using system("cls")
  * and on linux it is done using system("clear")
  * so i made a common variable CLEAR...system(CLEAR) to take "cls" if its
  * windows else take "clear if its linux */
 
-#ifdef __WIN32
+#ifdef __WIN32 /* if system is windows then __WIN32 would already be define so ifdef __WIN32 will be true */
 #define CLEAR "cls"
 #endif
 
-#ifdef linux
+#ifdef linux /* if system is linux then linux would be defined so ifdef linux would be true */
 #define CLEAR "clear"
 #endif
 
@@ -23,7 +23,6 @@ using namespace std;
 class StudentRegistration
 {
     public:
-        void inputName(string &name);
         void fillForm();
         void showDetails();
     private:
@@ -32,28 +31,16 @@ class StudentRegistration
         string contactNumber,emailID,rollNumber;
 };
 
-void StudentRegistration::inputName(string &name)
-{
-    string first,middle,last;
-    cout<<"\nFirst Name: ";
-    cin>>first;
-    cout<<"\nMiddle Name: ";
-    cin>>middle;
-    cout<<"\nLast Name: ";
-    cin>>last;
-    name = first + " " + middle + " " + last;
-}
-
 void StudentRegistration::fillForm()
 {
     cout<<"\nEnter Roll no: ";
-    cin>>rollNumber;
+    getline(cin,rollNumber); // getline function can take string input with spaces included...so user can enter full name
     cout<<"\nEnter Students Name: ";
-    inputName(studentName);
+    getline(cin,studentName);
     cout<<"\nEnter Fathers Name: ";
-    inputName(fatherName);
+    getline(cin,fatherName);
     cout<<"\nEnter Mothers Name: ";
-    inputName(motherName);
+    getline(cin,motherName);
     cout<<"\nEnter DOB: ";
     cout<<"\nDate: ";
     cin>>dobDate;
